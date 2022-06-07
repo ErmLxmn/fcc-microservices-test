02ddab9433bd8fdb1791802f58be3ae816295e85
Ermel Laxamana <Ermel.Laxamana@filamsoftware.com>
2022-06-07 09:47:41 +0800

    first submission

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
+const multer  = require('multer')
 var cors = require('cors');
 require('dotenv').config()
-
+const upload = multer({})
 var app = express();
 
 app.use(cors());
@@ -11,7 +12,14 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse", upload.single('upfile') ,function (req, res){
+  let response = {
+    name : req.file.originalname,
+    type : req.file.mimetype,
+    size : req.file.size
+  }
+  res.json(response)
+})
 
 
 const port = process.env.PORT || 3000;
